e80fae48be033fbea69858b64caafa496977c331
SebastienDaniel <sebastien.daniel@sebweb.ca>
2015-05-15 18:40:56 -0400

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,116 @@
+module.exports = function(grunt) {
+    // instructions for grunt
+    
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+        copy: {
+            dev: {
+                files:  [{
+                    expand: true,
+                    cwd: "src/scripts/",
+                    src: [ "**/*.js"],
+                    dest: "build/dev/assets/js/"
+                },
+                {
+                    expand: true,
+                    cwd: "src/img/",
+                    src: [ "**/*.{jpg,jpeg,png,gif}"],
+                    dest: "build/dev/assets/img/"
+                }]
+            }
+        },
+        jshint: {
+            src:  [ "src/scripts/**/*.js" ]
+        },
+        jscs: {
+            src: [ "src/scripts/**/*.js" ]
+        },
+        uglify: {
+            prod: {
+                options: {
+                    expand: true,
+                    mangle: true,
+                    compress: true,
+                    screwIE8: true
+                },
+                src: "src/scripts/**/<%= pkg.name %>.js",
+                dest: "build/prod/assets/js/<%= pkg.name %>.min.js"
+            }
+        },
+        qunit: {
+            dev: {
+                src: ["test/**/*.html"]
+            }
+        },
+        less: {
+            dev: { // parse all LESS files into their CSS equivalents
+                files: [
+                    {
+                        expand: true,
+                        cwd: "src/styles",
+                        src: [ "**/*.less" ],
+                        dest: "build/dev/assets/css",
+                        ext: ".css"
+                    }
+                ]
+            },
+            prod: {
+                options: {
+                    compress: true
+                },
+                files: [
+                    {
+                        expand: true,
+                        cwd: "src/styles",
+                        src: [ "**/*.less" ],
+                        dest: "build/prod/assets/css",
+                        ext: ".css"
+                    }
+                ]
+            }
+        },
+        jsdoc: {
+            src: ["src/scripts/*.js", "!src/scripts/polyfills.js"],
+            options: {
+                destination: "doc",
+                private: false
+            }
+        },
+        csslint: {
+            options: {
+                import: 2
+            },
+            src: ["build/dev/assets/css/**/*.css"]
+        },
+        lesslint: {
+            src: ["src/styles/**/*.less"],
+            options: {
+                csslint: {
+                    "adjoining-classes": false,
+                    "box-model": false,
+                    "unqualified-attributes": false,
+                    "unique-headings": false,
+                    "box-sizing": false,
+                    "outline-none": false,
+                    "font-sizes": false,
+                    "floats": false
+                }
+            }
+        }
+    });
+
+    // Load the plugins
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-contrib-jshint");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-jscs");
+    grunt.loadNpmTasks("grunt-contrib-qunit");
+    grunt.loadNpmTasks("grunt-contrib-less");
+    grunt.loadNpmTasks("grunt-jsdoc");
+    grunt.loadNpmTasks("grunt-contrib-csslint");
+    grunt.loadNpmTasks("grunt-lesslint");
+    
+    grunt.registerTask("dev", ["test", "less:dev", "copy:dev"]);
+    grunt.registerTask("prod", ["concat:prod", "cssmin:prod", "copy:prod"]);
+    grunt.registerTask("test", ["jshint", "jscs", "lesslint", "qunit"]);
+};
---
+++ src/scripts/ajax.js
@@ -0,0 +1,130 @@
+/**
+ * Created by Sebastien on 2015-02-19.
+ */
+
+var ajax = (function() {
+    "use strict";
+    var loader = document.createElement("div");
+    loader.className = "ajax-loader";
+
+    function loaderStart(target) {
+        target.appendChild(loader);
+    }
+
+    function loaderStop(target) {
+        target.removeChild(loader);
+    }
+
+    function sendRequest(c) {
+        var req = new XMLHttpRequest();
+
+        if (c.loaderTarget && c.loaderTarget.nodeType === 1) {
+            loaderStart(c.loaderTarget);
+        }
+
+        // start XMLHttpRequest
+        req.open(c.method, c.url, c.async);
+        setRequestHeaders(req, c);
+
+        // request now going through
+        req.onreadystatechange = function() {
+            // Headers & status are received
+            if (req.readyState === 2) {
+                if (c.onHeaders !== null) {
+                    c.onHeaders(req.getAllResponseHeaders());
+                }
+            }
+
+            // downloading data (partials are available)
+            //if (req.readyState === 3) {}
+
+            // request is complete and successful
+            if (req.readyState === 4) {
+                // loader needs to be removed BEFORE callbacks
+                if (c.loaderTarget && c.loaderTarget.nodeType === 1) {
+                    loaderStop(c.loaderTarget);
+                }
+                if ((req.status === 200 /* get */ || req.status === 201 /* post */ || req.status === 204) /* delete */ && c.onSuccess !== null) {
+                    c.onSuccess(getResponse(req, c), req.status);
+                } else {
+                    c.onFailure(getResponse(req, c), req.status);
+                }
+            }
+        };
+        req.send(c.data);
+    }
+
+    // set request headers based on c.headers
+    function setRequestHeaders(req, c) {
+        var headers = c.setHeaders || {};
+
+        // set default values
+        if (!c.setHeaders["Content-Type"]) {
+            c.setHeaders["Content-Type"] = "application/json;charset=utf-8";
+        }
+
+        if (!c.setHeaders.Accept) {
+            c.setHeaders.Accept = "application/json";
+        }
+
+        if (!c.setHeaders["X-Requested-With"]) {
+            c.setHeaders["X-Requested-With"] = "XMLHttpRequest";
+        }
+
+        // get all object (t) keys from own properties
+        // inject them as request headers
+        Object.keys(headers).forEach(function(header) {
+            req.setRequestHeader(header, headers[header]);
+        });
+
+        // xhr2 spec
+        if (req.responseType) {
+            req.responseType = c.responseType; // "arraybuffer", "blob", "document", "json", "text"
+        }
+    }
+
+    // parse response based on expected return type
+    function getResponse(req, c) {
+        var r = req.response;
+
+        if (c.responseType && c.responseType === "json" && r !== "") {
+            r = JSON.parse(r);
+        }
+
+        return r;
+    }
+
+    function buildRequest(config) {
+        // default config values
+        var c = {
+            async: config.async === false ? false : true,
+            data: config.data || null,
+            setHeaders: config.setHeaders || {},
+            method: config.method.toUpperCase() || "GET",
+            onFailure: config.onFailure || null,
+            onHeaders: config.onHeaders || null,
+            //onPartials: config.onPartials || null,
+            onSuccess: config.onSuccess || null,
+            responseType: config.responseType || "text",
+            url: encodeURI(config.url).replace(/%5B/g, "[").replace(/%5D/g, "]") || null,
+            loaderTarget: config.loadTarget || null
+        };
+
+        if (!c.url) {
+            throw new Error("ajax request requires a URL");
+        }
+
+        if (!c.method) {
+            throw new Error("ajax request requires a method (get,put,post,delete)");
+        }
+
+        if (Object.prototype.toString.call(c.setHeaders) !== "[object Object]") {
+            throw new TypeError("ajax request requires headers to be an object");
+        }
+
+        sendRequest(c);
+    }
+
+    // public API
+    return buildRequest;
+}());
