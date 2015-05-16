5411f92309584e731510a4e7c76d1556b5368515
SebastienDaniel <sebastien.daniel@sebweb.ca>
2015-05-16 15:23:41 -0400

    complete change of interface. ajax.get() ajax.post() ajax.put() ajax.delete() have been added as utility methods the direct ajax() call has been removed

--- src/scripts/ajax.js
+++ src/scripts/ajax.js
@@ -18,19 +18,19 @@ var ajax = (function() {
     function sendRequest(c) {
         var req = new XMLHttpRequest();
 
-        if (c.loaderTarget && c.loaderTarget.nodeType === 1) {
+        if (c.loaderTarget) {
             startLoader(c.loaderTarget);
         }
 
         // start XMLHttpRequest
         req.open(c.method, c.url, c.async);
-        setRequestHeaders(req, c);
-
+        setRequestHeaders(req, c.setHeaders);
+        
         // request now going through
         req.onreadystatechange = function() {
             // Headers & status are received
             if (req.readyState === 2) {
-                if (c.onHeaders !== null) {
+                if (c.onHeaders) {
                     c.onHeaders(req.getAllResponseHeaders());
                 }
             }
@@ -41,12 +41,14 @@ var ajax = (function() {
             // request is complete and successful
             if (req.readyState === 4) {
                 // loader needs to be removed BEFORE callbacks
-                if (c.loaderTarget && c.loaderTarget.nodeType === 1) {
+                if (c.loaderTarget) {
                     stopLoader(c.loaderTarget);
                 }
-                if ((req.status === 200 /* get */ || req.status === 201 /* post */ || req.status === 204) /* delete */ && c.onSuccess !== null) {
+
+                // process sucess or failure callbacks
+                if (req.status.toString().charAt(0) === "2" && c.onSuccess) {
                     c.onSuccess(getResponse(req, c), req.status);
-                } else {
+                } else if(c.onFailure) {
                     c.onFailure(getResponse(req, c), req.status);
                 }
             }
@@ -55,20 +57,18 @@ var ajax = (function() {
     }
 
     // set request headers based on c.headers
-    function setRequestHeaders(req, c) {
-        var headers = c.setHeaders || {};
-
+    function setRequestHeaders(req, headers) {
         // set default values
-        if (!c.setHeaders["Content-Type"]) {
-            c.setHeaders["Content-Type"] = "application/json;charset=utf-8";
+        if (!headers["Content-Type"]) {
+            headers["Content-Type"] = "application/json;charset=utf-8";
         }
 
-        if (!c.setHeaders.Accept) {
-            c.setHeaders.Accept = "application/json";
+        if (!headers.Accept) {
+            headers.Accept = "application/json";
         }
 
-        if (!c.setHeaders["X-Requested-With"]) {
-            c.setHeaders["X-Requested-With"] = "XMLHttpRequest";
+        if (!headers["X-Requested-With"]) {
+            headers["X-Requested-With"] = "XMLHttpRequest";
         }
 
         // get all object (t) keys from own properties
@@ -76,6 +76,7 @@ var ajax = (function() {
         Object.keys(headers).forEach(function(header) {
             req.setRequestHeader(header, headers[header]);
         });
+
 
         // xhr2 spec
         if (req.responseType) {
@@ -94,37 +95,72 @@ var ajax = (function() {
         return r;
     }
 
-    function buildRequest(config) {
-        // default config values
-        var c = {
-            async: config.async === false ? false : true,
-            data: config.data || null,
-            setHeaders: config.setHeaders || {},
-            method: config.method.toUpperCase() || "GET",
-            onFailure: config.onFailure || null,
-            onHeaders: config.onHeaders || null,
-            //onPartials: config.onPartials || null,
-            onSuccess: config.onSuccess || null,
-            responseType: config.responseType || "text",
-            url: encodeURI(config.url).replace(/%5B/g, "[").replace(/%5D/g, "]") || null,
-            loaderTarget: config.loadTarget || null
+    function validateConfig(c) {
+        if (Object.prototype.toString.call(c.url) !== "[object String]") {
+            throw new TypeError("ajax: requires a URL string");
+        }
+
+        if (c.loaderTarget && c.loaderTarget.nodeType !== 1) {
+            throw new TypeError("ajax: 'loaderTarget' is not an HTML element");
+        }
+
+        if (c.setHeaders && Object.prototype.toString.call(c.setHeaders) !== "[object Object]") {
+            throw new TypeError("ajax: 'setHeaders' is not an object");
+        }
+
+        if (c.onFailure && Object.prototype.toString.call(c.onFailure) !== "[object Function]") {
+            throw new TypeError("ajax: onFailure must be a function");
+        }
+        if (c.onSuccess && Object.prototype.toString.call(c.onFailure) !== "[object Function]") {
+            throw new TypeError("ajax: onFailure must be a function");
+        }
+        if (c.onHeaders && Object.prototype.toString.call(c.onFailure) !== "[object Function]") {
+            throw new TypeError("ajax: onFailure must be a function");
+        }
+
+        return true;
+    }
+
+    function finalizeConfig(c) {
+        return {
+            async: !!c.async,
+            data: c.data || null,
+            setHeaders: c.setHeaders || {},
+            method: c.method,
+            onFailure: c.onFailure || null,
+            onHeaders: c.onHeaders || null,
+            onSuccess: c.onSuccess || null,
+            responseType: c.responseType || "text",
+            url: encodeURI(c.url).replace(/%5B/g, "[").replace(/%5D/g, "]"),
+            loaderTarget: c.loadTarget || null
         };
-
-        if (!c.url) {
-            throw new Error("ajax request requires a URL");
-        }
-
-        if (!c.method) {
-            throw new Error("ajax request requires a method (get,put,post,delete)");
-        }
-
-        if (Object.prototype.toString.call(c.setHeaders) !== "[object Object]") {
-            throw new TypeError("ajax request requires headers to be an object");
-        }
-
-        sendRequest(c);
     }
 
     // public API
-    return buildRequest;
+    return {
+        "get": function(c) {
+            if (validateConfig(c)) {
+                c.method = "GET";
+                sendRequest(finalizeConfig(c));
+            }
+        },
+        "post": function(c) {
+            if (validateConfig(c)) {
+                c.method = "POST";
+                sendRequest(finalizeConfig(c));
+            }
+        },
+        "put": function(c) {
+            if (validateConfig(c)) {
+                c.method = "PUT";
+                sendRequest(finalizeConfig(c));
+            }
+        },
+        "delete": function(c) {
+            if (validateConfig(c)) {
+                c.method = "DELETE";
+                sendRequest(finalizeConfig(c));
+            }
+        }
+    };
 }());
